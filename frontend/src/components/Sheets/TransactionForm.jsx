207302fcb610dfa3079b1569f89812efbfb00de8
NyanLinHtet0 <harrishtet.nyanlinhtet@gmail.com>
2026-03-12 20:38:45 -0700

    last update

--- frontend/src/components/Sheets/TransactionForm.jsx
+++ frontend/src/components/Sheets/TransactionForm.jsx
@@ -6,6 +6,7 @@ export default function TransactionForm({ onSubmit, corpname }) {
   const [amount, setAmount] = useState('');
   const isBaht = corpname === 'Baht ဝယ်စာရင်း ()';
   const [rate, setRate] = useState('');
+  const [date, setDate] = useState(new Date().toISOString().split('T')[0]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -13,14 +14,15 @@ export default function TransactionForm({ onSubmit, corpname }) {
     if (isBaht){
       const calculatedTotal = Number(amount) * Number(rate);
       onSubmit({
+      date,
       description,
       amount: Number(amount),
-      rate: Number(rate),
-      totalAmount: calculatedTotal
+      rate: Number(rate)
     });}
     
     else{
       onSubmit({
+      date,
       description,
       amount: Number(amount)
     });}
@@ -28,7 +30,8 @@ export default function TransactionForm({ onSubmit, corpname }) {
 
     setDescription('');
     setAmount('');
-    setRate('')
+    setRate('');
+    setDate(new Date().toISOString().split('T')[0]);
   };
 
   return (
@@ -40,6 +43,13 @@ export default function TransactionForm({ onSubmit, corpname }) {
       <form onSubmit={handleSubmit} className={styles.formContainer}>
         {/* Horizontal Input Row */}
         <div className={styles.inputRow}>
+          <input
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            required
+            className={styles.flexInput}
+          />
           <input
             type="text"
             placeholder="Description"
